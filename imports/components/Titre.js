56cc765d0ca11d2b4c9ebb6aeb895add6d3f5a33
lGatl <gat55@live.fr>
2017-02-20 16:00:18 +0100

    TitreAudrey

--- imports/components/Titre.js
+++ imports/components/Titre.js
@@ -3,7 +3,7 @@ import { Segment } from 'semantic-ui-react'
 
 const Titre = () => (
   <div>
-    <Segment textAlign='center' inverted color='orange' compact padded floated = 'center' basic>
+    <Segment textAlign='center' inverted color='orange' compact padded floated = 'left' basic>
       <h1>Je suis le titre de chaque page !</h1>
     </Segment>
   </div>
